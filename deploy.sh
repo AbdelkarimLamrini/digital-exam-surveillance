1c2752618a1fd9da32fb0a4544861ab1ea8d713f
Abdelkarim Lamrini <abdelkarim.lamrini@outlook.com>
2024-03-29 01:34:46 +0100

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+echo "[1/4] - Stopping containers"
+docker compose down
+
+echo "[2/4] - Cleaning up volumes"
+rm -rf volumes/*
+mkdir -p volumes/postgres/data
+mkdir -p volumes/datarhei-core/config
+mkdir -p volumes/datarhei-core/data/recordings
+
+echo "[3/4] - Installing config files"
+cp restreamer-config.json volumes/restreamer/config/config.json
+
+echo "[4/4] - Launching new containers"
+docker compose up -d
+
+echo "Deployment finished successfully!"
